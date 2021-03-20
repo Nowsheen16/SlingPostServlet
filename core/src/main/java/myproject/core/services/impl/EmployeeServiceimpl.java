836b72d4fd25aac3a5cb3e89e7cf1d3633520068
Nowsheen16 <nowsheen.mufti@tothenew.com>
2021-03-21 00:29:07 +0530

    initial commit

---
+++ core/src/main/java/myproject/core/services/impl/EmployeeServiceimpl.java
@@ -0,0 +1,26 @@
+package myproject.core.services.impl;
+
+
+import myproject.core.services.EmployeeService;
+import myproject.core.services.EmployeeServiceConfig;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.metatype.annotations.Designate;
+
+@Component(service = EmployeeService.class)
+@Designate(ocd = EmployeeServiceConfig.class)
+public class EmployeeServiceimpl implements EmployeeService {
+    private String name;
+    private int id;
+
+    @Override
+    public String Name(String name) {
+        this.name = name;
+        return name;
+    }
+
+    @Override
+    public int id(int id) {
+        this.id = id;
+        return id;
+    }
+}
